07ccac3425318e6f1a428e0147d1f0eccfc9ec22
Martin Mochnacký <martin.mochnacky@student.tuke.sk>
2023-04-01 21:41:36 +0200

    chart.js -Martin

--- front-end/hack-kosice-app/src/app/main-content/main-content.component.ts
+++ front-end/hack-kosice-app/src/app/main-content/main-content.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables, ArcElement } from '../../../node_modules/chart.js/';
+
+import { DoughnutController } from '../../../node_modules/chart.js/';
+Chart.register(DoughnutController); // Register the doughnut controller
+
+Chart.register(ArcElement);
+
+
 
 @Component({
   selector: 'app-main-content',
@@ -10,6 +18,34 @@ export class MainContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const data = {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+
+    const myChart = new Chart('myChart', {
+      type: 'doughnut',
+      data: data,
+    });
+ 
+  }
+ 
+
+ 
   }
 
-}
+
